e8250cdeae879ec794271020955c814b46b3ab03
samferguson <fergusonsd@g.cofc.edu>
2019-09-04 12:48:21 -0400

    Make the script look nicer

--- exercises/myList-sam.sh
+++ exercises/myList-sam.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-echo "<DOCTYPE html><html><title>Current top level listng of directory $PWD</title><body> " > output.html
+echo "<!DOCTYPE html>" > output.html
+echo "<html>" >> output.html
+echo "    <head>" >> output.html
+echo "        <title>Current top level listing of directory $PWD</title>" >> output.html
+echo "    </head>" >> output.html
+echo "    <body>">> output.html
 for file in *; do
-	echo "<p>$file</p>" >> output.html
+    if [ "$file" = "output.html" ];
+    then
+        continue
+    fi
+    if test -d "$file"
+    then
+	    echo "        <p>$file/</p>" >> output.html
+    else
+        echo "        <p>$file</p>" >> output.html
+    fi
 done
-echo "</body></html>">> output.html
+echo "    </body>" >> output.html
+echo "</html>" >>output.html
 xdg-open output.html
