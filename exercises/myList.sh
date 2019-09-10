16fb13edd38af9b3a05f29f389b4fff4163d8b5c
samferguson <fergusonsd@g.cofc.edu>
2019-09-10 08:39:45 -0400

    Make Seth's list ours

--- exercises/myList.sh
+++ exercises/myList.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
-#sam
 
-echo "<!DOCTYPE html>" > output.html
-echo "<html>" >> output.html
-echo "    <head>" >> output.html
-echo "        <title>Current top level directory listing</title>" >> output.html
-echo "    </head>" >> output.html
-echo "    <body>">> output.html
-for file in ~/*; do
-    if [ "$file" = "output.html" ];
-    then
-        continue
-    fi
-    if test -d "$file"
-    then
-	    echo "        <p>$file/</p>" >> output.html
-    else
-        echo "        <p>$file</p>" >> output.html
-    fi
+echo "<DOCTYPE html><html>" > toplevel.html
+echo "<title>Top-Level Directory</title>" >> toplevel.html
+echo "<h1>Relative Top-Level Directory</h1>" >> toplevel.html
+
+for file in *; do
+	if [ "$file" = "toplevel.html" ]; then 
+		continue
+	fi	
+	echo "<p><a href="file:///$PWD/$file">$file</a></p>" >> toplevel.html
 done
-echo "    </body>" >> output.html
-echo "</html>" >>output.html
-xdg-open output.html
+
+echo "</html>" >> toplevel.html
+
+xdg-open toplevel.html
