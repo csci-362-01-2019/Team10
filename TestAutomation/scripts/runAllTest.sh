c593c301bf696b21dfe62313611989d26a37a7eb
Meagan <gouldma@g.cofc.edu>
2019-10-24 09:25:07 -0400

    update

--- TestAutomation/scripts/runAllTest.sh
+++ TestAutomation/scripts/runAllTest.sh
@@ -4,7 +4,7 @@ parentdir="$PWD"
 
 echo "\"data\"["
 for test_case in testCases/*; do
-	test_id = $(jq .'test_id' $test_id)
+	test_id=$(jq .'test_id' $test_case)
 	echo "\"test_id\": "\"$test_id"\","
 	import_dir="$parentdir/$(jq -r .'extra_path[]' $test_case)"
 	requirement=$(jq .'requirement' $test_case)
@@ -14,7 +14,7 @@ for test_case in testCases/*; do
 	method_tested=$(jq -r .'method_tested' $test_case)
 	echo "\"method_tested\": "\"$method_tested"\","
 	echo "\"inputs\": "\"$inputs"\","
-	expected_output = $(jq -r .'output' $test_Case)
+	expected_output=$(jq -r .'output' $test_Case)
 	echo "\"expected_output\": "\"$expected_output"\","
 	output=$(python $driver_name $inputs $import_dir)
 	echo "\"actual_output\": "\"$output"\","
