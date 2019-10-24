6ff0c94be5ca160e6f8941edbebc6703f17b4433
SamFerguson <fergusonsd@g.cofc.edu>
2019-10-24 09:37:13 -0400

    Add json ability to script

--- TestAutomation/scripts/runAllTest.sh
+++ TestAutomation/scripts/runAllTest.sh
@@ -2,26 +2,28 @@
 
 parentdir="$PWD"
 
-echo "\"data\"["
+echo "\"data\":[" > reports/output.json
 for test_case in testCases/*; do
-	test_id = $(jq .'test_id' $test_id)
-	echo "\"test_id\": "\"$test_id"\","
+	echo "{"  >> reports/output.json
+	test_id=$(jq .'test_id' $test_case)
+	echo "\"test_id\": "\"$test_id"\"," >> reports/output.json
 	import_dir="$parentdir/$(jq -r .'extra_path[]' $test_case)"
 	requirement=$(jq .'requirement' $test_case)
 	inputs=$(jq .'inputs[]' $test_case)
 	driver_name=$(jq -r .'driver_name' $test_case)
-	echo "\"driver_name\": "\"$driver_name"\","
+	echo "\"driver_name\": "\"$driver_name"\"," >> reports/output.json
 	method_tested=$(jq -r .'method_tested' $test_case)
-	echo "\"method_tested\": "\"$method_tested"\","
-	echo "\"inputs\": "\"$inputs"\","
-	expected_output = $(jq -r .'output' $test_Case)
-	echo "\"expected_output\": "\"$expected_output"\","
+	echo "\"method_tested\": "\"$method_tested"\"," >> reports/output.json
+	echo "\"inputs\": "\"$inputs"\"," >> reports/output.json
+	expected_output=$(jq -r .'output' $test_case)
+	echo "\"expected_output\": "\"$expected_output"\"," >> reports/output.json
 	output=$(python $driver_name $inputs $import_dir)
-	echo "\"actual_output\": "\"$output"\","
+	echo "\"actual_output\": "\"$output"\"" >> reports/output.json
+	echo "},"  >> reports/output.json
 	
 	
 
 
 done
 
-echo "]"
+echo "]" >> reports/output.json
