31a90157129946f4a07c29517258f5a7378554be
Meagan <gouldma@g.cofc.edu>
2019-10-24 09:20:32 -0400

    testing for json output format

--- TestAutomation/scripts/runAllTest.sh
+++ TestAutomation/scripts/runAllTest.sh
@@ -2,17 +2,26 @@
 
 parentdir="$PWD"
 
+echo "\"data\"["
 for test_case in testCases/*; do
+	test_id = $(jq .'test_id' $test_id)
+	echo "\"test_id\": "\"$test_id"\","
 	import_dir="$parentdir/$(jq -r .'extra_path[]' $test_case)"
-	echo "This is the import dir $import_dir"
-	echo "$test_case" 	
 	requirement=$(jq .'requirement' $test_case)
-	echo "This is our requirement: $requirement"
 	inputs=$(jq .'inputs[]' $test_case)
 	driver_name=$(jq -r .'driver_name' $test_case)
+	echo "\"driver_name\": "\"$driver_name"\","
+	method_tested=$(jq -r .'method_tested' $test_case)
+	echo "\"method_tested\": "\"$method_tested"\","
+	echo "\"inputs\": "\"$inputs"\","
+	expected_output = $(jq -r .'output' $test_Case)
+	echo "\"expected_output\": "\"$expected_output"\","
 	output=$(python $driver_name $inputs $import_dir)
-	echo "$output"
-	expected_output=$(jq .'output' $test_case)
+	echo "\"actual_output\": "\"$output"\","
+	
+	
 
 
 done
+
+echo "]"
