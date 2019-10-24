95edc4a7484fbcceac1ec06db0ceb635a7f4f562
SamFerguson <fergusonsd@g.cofc.edu>
2019-10-24 14:10:49 -0400

    Add new test cases and html renderer

---
+++ TestAutomation/reports/test.py
@@ -0,0 +1,21 @@
+import jinja2
+import sys
+import json
+import os
+
+
+def main():
+
+    with open("reports/output.json", "r") as data_file:
+        json_string = data_file.read()
+    json_obj = json.loads(json_string)
+
+    environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.getcwd()))
+    template_one = environment.get_template("reports/output_template.html")
+    rendered_html = template_one.render(data=json_obj)
+    output_file = open("reports/test_results.html","w")
+    output_file.write(rendered_html)
+    output_file.close()
+
+if __name__ == "__main__":
+    main()
